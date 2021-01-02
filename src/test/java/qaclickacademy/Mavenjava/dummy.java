667ef53a8045c077bb4249bd594b05300a0962d5
surabhi <surabhi.rathor31@gmail.com>
2021-01-02 11:01:48 +0530

    new commit

---
+++ src/test/java/qaclickacademy/Mavenjava/dummy.java
@@ -0,0 +1,9 @@
+package qaclickacademy.Mavenjava;
+
+public class dummy {
+
+	public void neW()
+	{
+		System.out.println("Hello Surabhi");
+	}
+}
--- src/test/java/qaclickacademy/Mavenjava/newdemo.java
+++
@@ -1,5 +0,0 @@
-package qaclickacademy.Mavenjava;
-
-public class newdemo {
-
-}
